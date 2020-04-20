b91d377f59edee4b101672d5da2b945da3cfdc0f
Kiera Allen <kieraaallen@gmail.com>
2020-04-20 17:13:53 -0600

    Add encrypt and decrypt tests using interaction pattern

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -9,27 +9,28 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, enigma
   end
 
+  def test_it_can_encrypt_using_key_and_date
+    enigma = Enigma.new
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_can_decrypt_using_key_and_date
+    enigma = Enigma.new
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+
 end
 
-
-# pry(main)> require 'date'
-# #=> true
-#
-# pry(main)> require './lib/enigma'
-# #=> true
-#
-# pry(main)> enigma = Enigma.new
-# #=> #<Enigma:0x00007ff90f24cb78...>
-#
-# # encrypt a message with a key and date
-# pry(main)> enigma.encrypt("hello world", "02715", "040895")
-# #=>
-# #   {
-# #     encryption: "keder ohulw",
-# #     key: "02715",
-# #     date: "040895"
-# #   }
-#
 # # decrypt a message with a key and date
 # pry(main) > enigma.decrypt("keder ohulw", "02715", "040895")
 # #=>
