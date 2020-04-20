bf266dc289a20faf471babef6cb3df2520c3a0d2
Kiera Allen <kieraaallen@gmail.com>
2020-04-20 15:53:57 -0600

    Test Encryptor can create characters

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,2 +1,7 @@
 class Encryptor
+  attr_reader :characters
+
+  def initialize
+    @characters = ("a".."z").to_a << " "
+  end
 end
--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -2,8 +2,17 @@ require_relative 'test_helper'
 require './lib/encryptor'
 
 class EncryptorTest < Minitest::Test
+
   def test_it_exists
     encryptor = Encryptor.new
+
     assert_instance_of Encryptor, encryptor
+  end
+
+  def test_it_can_create_characters
+    encryptor = Encryptor.new
+    expected = ("a".."z").to_a << " "
+
+    assert_equal expected, encryptor.characters
   end
 end
