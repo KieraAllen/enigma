bafe18574001132c4dda7366a781c31d586c27b7
Kiera Allen <kieraaallen@gmail.com>
2020-04-20 15:58:01 -0600

    Remove setup

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -3,13 +3,12 @@ require 'date'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
-  def setup
-    @enigma = Enigma.new
-  end
 
   def test_it_exists
-    assert_instance_of Enigma, @enigma
+    enigma = Enigma.new
+    assert_instance_of Enigma, enigma
   end
+
 end
 
 
